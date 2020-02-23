9cd750f4044074b2562a14669b13f94865256625
Aleksander Czapski <alekczapski@gmail.com>
2020-02-23 09:47:31 +0100

    Start work on EC2 provisioning

--- main.go
+++ main.go
@@ -11,22 +11,31 @@ import (
 )
 
 func main() {
-	var accessToken string
+	var accessKey string
+	var secretKey string
 	var userDataFile string
 	var userdata string
 	var hostname string
+	var region string
 
-	flag.StringVar(&accessToken, "access-token", "", "Access token for provisioning a host")
+	flag.StringVar(&accessKey, "access-key", "", "Access key for provisioning a host")
+	flag.StringVar(&secretKey, "secret-key", "", "Secret key for provisioning a host")
 	flag.StringVar(&userDataFile, "userdata-file", "", "Apply user-data from a file to configure the host")
 	flag.StringVar(&hostname, "hostname", "provision-example", "Name for the host")
+	flag.StringVar(&region, "region", "eu-west-1", "Region for the host")
 	flag.Parse()
 
-	if len(accessToken) == 0 {
-		fmt.Fprintf(os.Stderr, "--access-token required")
+	if len(accessKey) == 0 {
+		fmt.Fprintf(os.Stderr, "--access-key required")
 		os.Exit(1)
 	}
 
-	provisioner, err := provision.NewDigitalOceanProvisioner(accessToken)
+	if len(secretKey) == 0 {
+		fmt.Fprintf(os.Stderr, "--secret-key required")
+		os.Exit(1)
+	}
+
+	provisioner, err := provision.NewEC2Provisioner(region, accessKey, secretKey)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
 		os.Exit(1)
@@ -45,9 +54,9 @@ func main() {
 	// Find examples here for other clouds -> https://github.com/inlets/inletsctl/blob/356886f41e7c48a9644a24532027d1defa1d69e8/cmd/create.go
 	res, err := provisioner.Provision(provision.BasicHost{
 		Name:       hostname,
-		OS:         "ubuntu-16-04-x64",
-		Plan:       "512mb",
-		Region:     "lon1",
+		OS:         "amzn2-ami-hvm-2.0.20191217.0-x86_64-gp2",
+		Plan:       "t2.micro",
+		Region:     region,
 		UserData:   userdata,
 		Additional: map[string]string{},
 	})
